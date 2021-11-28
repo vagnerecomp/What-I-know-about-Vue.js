5e3ff8fb933c67ad24834e5416615cf6c94df396
Vagner José <vagnerecomp@gmail.com>
2021-11-28 00:28:48 -0300

    Criando e usando componetes vue.js

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Olá {{frame}}!</h1>
+</template>
+
+<script>
+export default{
+    name: 'PrimeiroComponente',
+    data() {
+        return{
+            frame: 'Angular'
+        }
+    }
+}
+</script>
---
+++ src/components/SegundoComponente.vue
@@ -0,0 +1,11 @@
+<template>
+    <p><br>Chuuuupa React</p>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default{
+    name: 'SegundoComponente'
+    
+}
+</script>
