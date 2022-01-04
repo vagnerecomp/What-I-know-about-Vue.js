1f7fef55e64c447d426221408d598c3b3b9bf069
Vagner José <vagnerecomp@gmail.com>
2022-01-03 21:32:00 -0300

    Trabalhando com emit

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- Trabalhand com emit -->
+    <div>
+        <button @click="$emit('mudarImagem')">Trocar imagem do perfil</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MudarImagem',
+    emits: ['mudarImagem'] 
+}
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,14 +1,32 @@
 <template>
-<!-- // Conceitos abordados: Argumentos dinâmicos -->
-    <img :src="avatar" :alt="descricao">
+<!-- // Conceitos abordados: Argumentos dinâmicos, emit -->
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 <script>
+import MudarImagem from './MudarImagem.vue'
 export default{
     name: 'Picture',
     data(){
         return{
             avatar: "/img/avatar.png",
-            descricao: 'Vag profile picture'
+            descricao: 'Vag profile picture',
+            trocado: false
+        }
+    },
+    components: {
+        MudarImagem
+    },
+    methods: {
+        trocarImagem(){
+            this.trocado = !this.trocado
+            if(!this.trocado){
+                this.avatar = "/img/avatar2.png"
+            }else{
+                this.avatar = this.avatar = "/img/avatar.png"
+            }
         }
     }
 }
